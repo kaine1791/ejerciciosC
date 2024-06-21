2b451b534456924dbd7e9783146f3cfb6b93b97b
kaine1791 <64537213+kaine1791@users.noreply.github.com>
2024-06-21 07:47:46 +0200

    Create contador_de_digitos.c
    Escribir un programa que solicite la carga de un número entre 0 y 999,
    y nos muestre un mensaje de cuántos dígitos tiene el mismo.
    Finalizar el programa cuando se cargue el valor 0.

---
+++ do_while/contador_de_digitos.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+Escribir un programa que solicite la carga de un número entre 0 y 999,
+y nos muestre un mensaje de cuántos dígitos tiene el mismo.
+Finalizar el programa cuando se cargue el valor 0.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int numero,digitos,i;
+   
+   i=0;
+   
+   do{
+        printf("Ingrese un numero entre 0 y 999: ");
+        scanf("%d",&numero);
+        
+        if(numero<10 && numero>0){
+            digitos=1;
+            printf("Numero de digitos: %d \n",digitos);
+        }else if(numero>=10 && numero<100){
+            digitos=2;
+            printf("Numero de digitos: %d \n",digitos);
+        }else if(numero>=100){
+            digitos=3;
+            printf("Numero de digitos: %d \n",digitos);
+        }else if(numero==0){
+            printf("Programa finalizado. \n");
+        }
+   }while(numero != 0);
+    
+  
+    return 0;
+}
