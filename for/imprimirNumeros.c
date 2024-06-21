4706f6a14b15178ac1fe3dd97556b349d469fd83
kaine1791 <64537213+kaine1791@users.noreply.github.com>
2024-06-21 17:10:07 +0200

    Create imprimirNumeros.c
    Realizar un programa que imprima en pantalla los números del 1 al 100.

---
+++ for/imprimirNumeros.c
@@ -0,0 +1,19 @@
+/*
+    Realizar un programa que imprima en pantalla los números del 1 al 100.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    int i;
+
+    for(i=1;i<=100;i++){
+
+        printf(" - %d - ",i);
+
+    }
+
+
+    return 0;
+}
