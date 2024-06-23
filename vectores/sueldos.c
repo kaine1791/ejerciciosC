1c040ca179346845e67e30df4dd18fe4430cfb92
kaine1791 <64537213+kaine1791@users.noreply.github.com>
2024-06-23 06:43:35 +0200

    Update sueldos.c
    Se desea guardar los sueldos de 5 operarios.
    Según lo conocido deberíamos definir 5 variables
    si queremos tener en un cierto momento los 5 sueldos
    almacenados en memoria.
    Empleando un vector solo se requiere definir un único nombre y accedemos
    a cada elemento por medio del subíndice.

--- vectores/sueldos.c
+++ vectores/sueldos.c
@@ -1,31 +1,20 @@
-/******************************************************************************
-Se desea guardar los sueldos de 5 operarios.
-Según lo conocido deberíamos definir 5 variables
-si queremos tener en un cierto momento los 5 sueldos
-almacenados en memoria.
-Empleando un vector solo se requiere definir un único nombre y accedemos
-a cada elemento por medio del subíndice.
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
 {
-  int sueldos[4];
-  int i,j;
-  
-  
-  for(i=0;i<=4;++i){
-      
-      printf("Sueldo del %d empleado: ",i+1);
-      scanf("%d",&sueldos[i]);
-  }
-  
-  for(j=0;j<=4;++j){
-      printf("El sueldo del %d empleado es: %d \n",j+1,sueldos[j]);
-      
-  }
+   float sueldo[5];
+   int i;
    
+   for(i=0;i<=4;++i){
+       printf("Introduzca el %d sueldo: ",i+1);
+       scanf("%f",&sueldo[i]);
+   }
+   
+   printf("\n");
+   
+   for(i=0;i<=4;++i){
+       printf("El sueldo del %d trabajador es: %.2f \n",i+1,sueldo[i]);
+   }
 
     return 0;
 }
